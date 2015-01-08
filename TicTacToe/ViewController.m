6edbfd98f3717040bbc3b7a51b0050e6dc251025
Syed Amaanullah <syedamaanullah@Syeds-MacBook-Pro.local>
2015-01-08 17:38:26 -0600

    Enables a user to win the game Disabled the overide other stuff

--- TicTacToe/ViewController.m
+++ TicTacToe/ViewController.m
@@ -34,28 +34,74 @@
 
 - (IBAction)onLabelTapped:(UITapGestureRecognizer *)sender {
     CGPoint point = [sender locationInView:self.view];
+    NSString *canPlay = [NSString new];
 
    NSArray *labels = [NSArray arrayWithObjects:self.labelOne, self.labelTwo, self.labelThree, self.labelFour, self.labelFive, self.labelSix, self.labelSeven, self.labelEight, self.labelNine, nil];
 
     for (UILabel *label in labels) {
 
         if (CGRectContainsPoint(label.frame, point)) {
-            label.text = self.whichPlayerLabel.text;
+            if ([label.text isEqualToString:canPlay]) {
+                NSLog(@"canPlay");
+                label.text = self.whichPlayerLabel.text;
+                label.textColor = self.whichPlayerLabel.textColor;}
+        }
+    }
 
-            if ([self.whichPlayerLabel.text isEqualToString: @"o"]) {
-                self.whichPlayerLabel.text = @"x";
+    if ([self.whichPlayerLabel.text isEqualToString: @"o"]) {
+        self.whichPlayerLabel.text = @"x";
+        self.whichPlayerLabel.textColor = [UIColor blueColor];
 
-            }
+    } else {
+        self.whichPlayerLabel.text = @"o";
+        self.whichPlayerLabel.textColor = [UIColor redColor];
 
-            if ([self.whichPlayerLabel.text isEqualToString:@"x"]) {
-                self.whichPlayerLabel.text = @"o";
-            }
-        }}
+    }
 
+    //Determines winner
+    NSString *whoWon = [NSString new];
+    if ([self.labelOne.text isEqualToString:self.labelTwo.text] && [self.labelTwo.text isEqualToString:self.labelThree.text]) {
+        whoWon = self.labelOne.text;
+        NSLog(@"%@", whoWon);
+    }
 
+    if ([self.labelFour.text isEqualToString:self.labelFive.text] && [self.labelFive.text isEqualToString:self.labelSix.text]) {
+        whoWon = self.labelFour.text;
+        NSLog(@"%@", whoWon);
+    }
+
+    if ([self.labelSeven.text isEqualToString:self.labelEight.text] && [self.labelEight.text isEqualToString:self.labelNine.text]) {
+        whoWon = self.labelSeven.text;
+        NSLog(@"%@", whoWon);
+    }
+
+    if ([self.labelOne.text isEqualToString:self.labelFour.text] && [self.labelFour.text isEqualToString:self.labelSeven.text]) {
+        whoWon = self.labelOne.text;
+        NSLog(@"%@", whoWon);
+    }
+
+    if ([self.labelTwo.text isEqualToString:self.labelFive.text] && [self.labelFive.text isEqualToString:self.labelEight.text]) {
+        whoWon = self.labelTwo.text;
+        NSLog(@"%@", whoWon);
+    }
+
+    if ([self.labelThree.text isEqualToString:self.labelSix.text] && [self.labelSix.text isEqualToString:self.labelNine.text]) {
+        whoWon = self.labelThree.text;
+        NSLog(@"%@", whoWon);
+    }
+
+    if ([self.labelOne.text isEqualToString:self.labelFive.text] && [self.labelFive.text isEqualToString:self.labelNine.text]) {
+        whoWon = self.labelOne.text;
+        NSLog(@"%@", whoWon);
+    }
+
+    if ([self.labelThree.text isEqualToString:self.labelFive.text] && [self.labelFive.text isEqualToString:self.labelSeven.text]) {
+        whoWon = self.labelThree.text;
+        NSLog(@"%@", whoWon);
+    }
 }
 
 
+
+
 @end
-
-
